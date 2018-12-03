293173767e963b67442b081b6b37fda499573732
balde-isc <baldemar.isc@gmail.com>
2018-12-03 13:40:19 -0600

    PlatziverseDB: creando los modelos

--- platziverse/platziverse-db/index.js
+++ platziverse/platziverse-db/index.js
@@ -1,4 +1,7 @@
 'use strict'
+  const setUpDatabase = require('./lib/db');
+  const setUpAgentModel = require('./models/agent');
+  const setUpMetricModel = require('./models/metric');
 
 module.exports = async function (config) {
   const Agent = {}
